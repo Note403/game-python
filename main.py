5f0a073fcb5cad5cb3374d26ea8f13271be74742
Note403 <83089664+Note403@users.noreply.github.com>
2022-08-29 08:10:08 +0200

    basic pygame setup

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello")
+import sys
+
+import pygame
+
+pygame.init()
+
+
+class Game:
+    size = height, width = 720, 480
+    display = pygame.display.set_mode(size)
+    player = pygame.Rect(0, 0, 50, 100)
+    running = True
+
+    def __init__(self):
+        pygame.display.set_caption("GAME")
+
+    def game(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            pygame.draw.rect(self.display, (0, 200, 0), self.player)
+            pygame.display.flip()
+
+        sys.exit()
+
+
+game = Game()
+game.game()
