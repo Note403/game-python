e067c7e9a1faa56945bc6ec35668da698fa782b5
Note403 <83089664+Note403@users.noreply.github.com>
2022-09-07 09:36:16 +0200

    icon and player

--- main.py
+++ main.py
@@ -2,48 +2,86 @@ import sys
 
 import pygame
 from player.player import Player
-from enemy.enemy import Enemy
 
 pygame.init()
-
-
-window = pygame.display.set_mode((720, 480))
+pygame.font.init()
+pygame.key.set_repeat(True)
 
 
 class Game:
     def __init__(self):
         pygame.display.set_caption("GAME")
         self.size = height, width = 720, 480
-
         self.display = pygame.display.set_mode(self.size)
 
         self.player = Player()
-        self.enemy = Enemy()
 
         self.running = True
         self.clock = pygame.time.Clock()
 
         self.sprites = pygame.sprite.Group()
         self.sprites.add(self.player)
-        self.sprites.add(self.enemy)
+
+        self.fps_font = pygame.font.SysFont('Arial', 15)
+
+        self.background = pygame.image.load('assets/background.png')
+        self.background = pygame.transform.scale(
+            self.background,
+            (self.background.get_width() * 2.5, self.background.get_height() * 2.5)
+        )
+
+        self.bg_data = {
+            'min_x': 0 - (self.display.get_width() / 2)
+        }
+
+        self.bg_min_x = 0 - (self.display.get_width() / 2)
+        self.bg_min_y = 0 - (self.display.get_width() / 2)
+
+        self.bg_max_x = self.bg_min_x + self.display.get_width()
+        self.bg_max_y = self.bg_min_y + self.display.get_height()
+
+        self.icon = pygame.image.load('assets/icon.gif')
+        pygame.display.set_icon(self.icon)
+
+    def draw_camera(self):
+        if (self.player.f_x - (self.display.get_width() / 2)) <= self.bg_min_x:
+            bg_x = 0
+
+            if self.player.f_x >= self.bg_min_x:
+                player_x = (self.display.get_width() / 2) + self.player.f_x
+            else:
+                player_x = self.bg_min_x
+                self.player.f_x = self.bg_min_x + 1
+        else:
+            bg_x = 0 - self.player.f_x
+            player_x = self.display.get_width() / 2
+
+        self.display.blit(self.background, (bg_x, 0 - self.player.f_y))
+        self.display.blit(self.player.image, [player_x, self.display.get_height() / 2])
 
     def game(self):
-
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
 
+                keys_pressed = pygame.key.get_pressed()
+
+                if self.player.in_attack:
+                    self.player.handle_attack(keys_pressed, pygame.time.get_ticks())
+
                 if event.type == pygame.KEYDOWN:
-                    keys_pressed = pygame.key.get_pressed()
-                    self.player.handle_movement(keys_pressed)
+                    self.player.handle_movement(keys_pressed, pygame.time.get_ticks())
 
-            player = self.player.get_loc()
-
-            self.enemy.move_towards_player(player, pygame.time.get_ticks())
             self.sprites.update()
-            self.display.fill((0, 0, 0))
-            self.sprites.draw(self.display)
+            self.draw_camera()
+
+            for i in range(self.player.lifes):
+                self.display.blit(self.player.heart_sprite, (i * 20, 0))
+
+            self.display.blit(self.fps_font.render(str(self.clock.get_fps()), False, (255, 255, 255)), (0, 30))
+            self.display.blit(self.fps_font.render("PLAYER_X: " + str(self.player.f_x), False, (255, 255, 255)), (0, 50))
+            self.display.blit(self.fps_font.render("PLAYER_Y: " + str(self.player.f_y), False, (255, 255, 255)), (0, 600))
             pygame.display.flip()
             self.clock.tick(60)
 
--- player/player.py
+++ player/player.py
@@ -4,26 +4,162 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(
-            'assets/player_sprite.png').convert().subsurface(0, 38, 16, 16)
+        self.sprite_img = pygame.image.load('assets/player_sprite.png').convert()
+        self.image = self.sprite_img.subsurface(0, 38, 16, 16)
         self.image = pygame.transform.scale(self.image, (36, 36))
+        self.heart_sprite = pygame.image.load('assets/heart.png').convert()
+        self.heart_sprite = pygame.transform.scale(self.heart_sprite, (20, 20))
         self.rect = self.image.get_rect()
 
-        self.velocity = 3
+        self.lifes = 4
 
-    def handle_movement(self, keys_pressed):
+        self.f_x = 0.0
+        self.f_y = 0.0
+
+        self.velocity = 0.3
+
+        # ANIMATIONS
+
+        self.animation_index = 0
+        self.last_change_tick = 0
+        self.last_direction = 's'
+
+        self.last_attack = 0
+        self.in_attack = False
+
+        self.attack_sprites = {
+            'w': self.sprite_img.subsurface(50, 0, 16, 16),
+            'a': self.sprite_img.subsurface(48, 55, 16, 16),
+            's': self.sprite_img.subsurface(50, 39, 16, 16),
+            'd': self.sprite_img.subsurface(50, 20, 16, 16)
+        }
+
+        self.image = self.attack_sprites['d']
+        self.image = pygame.transform.scale(self.image, (36, 36))
+
+        self.animation_left = [
+            self.sprite_img.subsurface(0, 56, 16, 16),
+            self.sprite_img.subsurface(32, 56, 16, 16),
+        ]
+
+        self.animation_right = [
+            self.sprite_img.subsurface(0, 20, 16, 16),
+            self.sprite_img.subsurface(32, 20, 16, 16),
+        ]
+
+        self.animation_forwards = [
+            self.sprite_img.subsurface(0, 2, 16, 16),
+            self.sprite_img.subsurface(32, 2, 16, 16),
+        ]
+
+        self.animation_backwards = [
+            self.sprite_img.subsurface(0, 38, 16, 16),
+            self.sprite_img.subsurface(32, 38, 16, 16),
+        ]
+
+    def handle_movement(self, keys_pressed, tick):
+        self.handle_attack(keys_pressed, tick)
+
+        if self.in_attack:
+            return
+
         if keys_pressed[pygame.K_w]:
-            self.rect.y -= self.velocity
+            if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_a]:
+                self.f_y -= self.velocity / 2
+            else:
+                self.f_y -= self.velocity
+
+            self.last_direction = 'w'
+            self.animate('w', tick)
 
         if keys_pressed[pygame.K_s]:
-            self.rect.y += self.velocity
+            if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_a]:
+                self.f_y += self.velocity / 2
+            else:
+                self.f_y += self.velocity
+
+            self.last_direction = 's'
+            self.animate('s', tick)
 
         if keys_pressed[pygame.K_a]:
-            self.rect.x -= self.velocity
+            if keys_pressed[pygame.K_s] or keys_pressed[pygame.K_w]:
+                self.f_x -= self.velocity / 2
+            else:
+                self.f_x -= self.velocity
+
+            self.last_direction = 'a'
+            self.animate('a', tick)
 
         if keys_pressed[pygame.K_d]:
-            self.rect.x += self.velocity
+            if keys_pressed[pygame.K_s] or keys_pressed[pygame.K_w]:
+                self.f_x += self.velocity / 2
+            else:
+                self.f_x += self.velocity
 
-    def get_loc(self):
-        player_location = (self.rect.x, self.rect.y)
-        return player_location
+            self.last_direction = 'd'
+            self.animate('d', tick)
+
+    def animate(self, w_direction, tick):
+        if w_direction == 'w':
+            self.change_sprite_index(tick)
+
+            self.image = self.animation_forwards[self.animation_index]
+            self.image = pygame.transform.scale(self.image, (36, 36))
+
+        if w_direction == 's':
+            self.change_sprite_index(tick)
+
+            self.image = self.animation_backwards[self.animation_index]
+            self.image = pygame.transform.scale(self.image, (36, 36))
+
+        if w_direction == 'a':
+            self.change_sprite_index(tick)
+
+            self.image = self.animation_left[self.animation_index]
+            self.image = pygame.transform.scale(self.image, (36, 36))
+
+        if w_direction == 'd':
+            self.change_sprite_index(tick)
+
+            self.image = self.animation_right[self.animation_index]
+            self.image = pygame.transform.scale(self.image, (36, 36))
+
+    def change_sprite_index(self, tick):
+        if tick > (self.last_change_tick + 250):
+            if self.animation_index == 1:
+                self.animation_index = 0
+                self.last_change_tick = tick
+            else:
+                self.animation_index = 1
+                self.last_change_tick = tick
+
+    def handle_attack(self, keys_pressed, tick):
+        if self.in_attack:
+            if self.last_attack + 55 < tick:
+                self.in_attack = False
+                self.load_old_sprite()
+
+            return
+
+        if self.last_attack + 200 > tick:
+            return
+
+        if keys_pressed[pygame.K_SPACE]:
+            self.image = self.attack_sprites[self.last_direction]
+            self.image = pygame.transform.scale(self.image, (36, 36))
+            self.in_attack = True
+            self.last_attack = tick
+
+    def load_old_sprite(self):
+        self.animation_index = 0
+
+        if self.last_direction == 'w':
+            self.image = self.animation_forwards[self.animation_index]
+        elif self.last_direction == 'a':
+            self.image = self.animation_left[self.animation_index]
+        elif self.last_direction == 's':
+            self.image = self.animation_backwards[self.animation_index]
+        elif self.last_direction == 'd':
+            self.image = self.animation_right[self.animation_index]
+
+        self.image = pygame.transform.scale(self.image, (36, 36))
